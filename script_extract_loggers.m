5263618d150a274fe130acb0e2b6660dc425840c
Julie E Elie <julieelie@users.noreply.github.com>
2018-09-11 16:42:16 -0700

    resolving filename issues

--- script_extract_loggers.m
+++ script_extract_loggers.m
@@ -1,10 +1,28 @@
-Input_folder='Z:\users\Julie E\GiMo_65430_71300\Loggers\06252018';
+%% Inputs
+Date = '06252018';
 % BatIDs = {'65430' '71300' '71335' '71132' '59899' '65430'};
 BatIDs = {'60014' '65430' '71300' '71335' '71137' '65430'};
 % LoggerNum = [5 6 7 9 10 16];
 LoggerNum = [3 5 6 7 9 16];
+
+%% Set paths and dependencies
 addpath(genpath('C:\Users\Julie\Documents\GitHub\LoggerDataProcessing'))
-Logger_dirs = dir(fullfile(Input_folder, 'logger*'));
+Server_path = 'Z:\users\Julie E\GiMo_65430_71300\Loggers';
+Local_path = 'C:\Users\Julie\Documents\TempData';
+Input_serverfolder=fullfile(Server_path, Date);
+Input_localfolder = fullfile(Local_path, Date);
+
+%% Bring data back on the computer
+mkdir(Input_localfolder)
+[s,m,e]=copyfile(Input_serverfolder, Input_localfolder);
+if ~s
+    m
+    e
+    error('File transfer did not occur correctly for %s\n', Date);
+end
+
+%% Run
+Logger_dirs = dir(fullfile(Input_localfolder, 'logger*'));
 NLogger = length(Logger_dirs);
 Input_dir = cell(1,NLogger);
 
