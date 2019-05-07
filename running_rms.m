6cb25f2354effeaf82ecd597ba233d823eb1b452
Julie Elie <julie.elie@gmail.com>
2019-05-06 17:43:10 -0700

    improve the treatment of inputs to running_rms

--- running_rms.m
+++ running_rms.m
@@ -1,4 +1,4 @@
-function [Amp_env_voltage]=running_rms(Filtered_voltage_trace, FS_in, Fhigh_power, FS_env, FigFlag)
+function [Amp_env_voltage, Power_env]=running_rms(Filtered_voltage_trace, FS_in, Fhigh_power, FS_env, FigFlag)
 if nargin<5
     FigFlag=0;
 end
@@ -6,26 +6,24 @@ if nargin<4
     FS_env = 100; % sampling frequency of the output running RMS set to be 100Hz (1 sample per 10ms)
 end
 if nargin<3
-    Fhigh_power = 20; % frequency value of the low-pass filter on voltage signal power
+    Fhigh_power = 100; % frequency value of the low-pass filter on voltage signal power
+end
+Nfilt = 2^ceil(log2(2*FS_in/Fhigh_power));
+Nframes = length(Filtered_voltage_trace);
+if Nframes<3*Nfilt
+    error('The signal is too short to calculate an enveloppe with such a low frequency for the low-pass filter on signal power!\n')
 end
 
-Nframes = length(Filtered_voltage_trace);
-if ( Nframes > 3*512 )
-    Nfilt = 512;
-elseif ( Nframes > 3*64 )
-    Nfilt = 64;
-    
-elseif ( Nframes > 3*16 )
-    Nfilt = 16;
-else
-    error('Data section is too short for filtering');
-end
 % Generate filter and filter signal power
 Lowpass_filter = fir1(Nfilt, Fhigh_power*2.0/FS_in);
-Amp_env_voltage = (filtfilt(Lowpass_filter, 1, Filtered_voltage_trace.^2)).^.5;
+Power_env = filtfilt(Lowpass_filter, 1, Filtered_voltage_trace.^2);
+Filtered_env = Power_env;
+Filtered_env(Filtered_env<0) = 0;
+Amp_env_voltage = Filtered_env.^.5;
 % Resample to desired sampling rate
 if FS_in ~= FS_env
     Amp_env_voltage = resample(Amp_env_voltage, FS_env, round(FS_in));
+    Power_env = resample(Power_env, FS_env, round(FS_in));
 end
 if FigFlag
     figure()
